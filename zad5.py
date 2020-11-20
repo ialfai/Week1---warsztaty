75ea6ec0700c3053d7a2bcc414f005f0c018e977
ialfai <k.zgodaferchmin@gmail.com>
2020-11-20 17:06:48 +0200

    zad 5 works - done

---
+++ zad5.py
@@ -0,0 +1,70 @@
+# W grach planszowych i papierowych RPG używa się wielu rodzajów kostek do gry,
+# nie tylko tych dobrze znanych, sześciennych.
+# Jedną z popularniejszych kości jest np. kostka dziesięciościenna, a nawet stuścienna!
+# Ponieważ w grach kośćmi rzuca się często, pisanie za każdym razem np.
+# "rzuć dwiema kostkami dziesięciościennymi, a do wyniku dodaj 20" byłoby nudne, trudne i
+# marnowałoby ogromne ilości papieru. W takich sytuacjach używa się kodu "rzuć 2D10+20".
+#
+# Kod takiej kostki wygląda następująco:
+# xDy+z
+#
+# gdzie:
+#
+#     y – rodzaj kostek, których należy użyć (np. D6, D10),
+#     x – liczba rzutów kośćmi; jeśli rzucamy raz, ten parametr jest pomijalny,
+#     z – liczba, którą należy dodać (lub odjąć) do wyniku rzutów (opcjonalnie).
+#
+# Przykłady:
+#
+#     2D10+10: 2 rzuty D10, do wyniku dodaj 10,
+#     D6: zwykły rzut kostką sześcienną,
+#     2D3: rzut dwiema kostkami trójściennymi,
+#     D12-1: rzut kością D12, od wyniku odejmij 1.
+#
+# Napisz funkcję, która:
+#
+#     przyjmie w parametrze taki kod w postaci łańcucha znaków,
+#     rozpozna wszystkie dane wejściowe:
+#         rodzaj kostki,
+#         liczbę rzutów,
+#         modyfikator,
+#
+#
+#     jeśli podany ciąg znaków jest niepoprawny, zwróci odpowiedni komunikat, - ok
+#     wykona symulację rzutów i zwróci wynik.
+#
+# Typy kostek występujące w grach: D3, D4, D6, D8, D10, D12, D20, D100. - ok
+
+import random
+
+def dice():
+    dice_sum = 0
+    code = input('Submit the code: ')
+    if "D" not in code:
+        print('Wrong code')
+    else:
+        code_splited = list(code.split("D"))
+        print(code_splited)
+        x = int(code_splited[0])
+        print('x: ', x)
+        y = int(code_splited[1].replace("+", "-").split("-")[0])
+        print('y: ', y)
+        if y not in [3, 4, 6, 8, 10, 12, 20, 100]:
+            print('Wrong code')
+        else:
+            if "+" in code:
+                z = int(code[-2:])
+            elif "-" in code:
+                z = int(code[-2:])
+            else:
+                z = 0
+            print('z: ', z)
+            for i in range(0, x):
+                throw = random.randint(1, y)
+                print('throw: ', throw)
+                dice_sum += throw
+                print('dice sum: ', dice_sum)
+            return dice_sum + z
+
+print(dice())
+
