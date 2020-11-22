ea0ec9b49efad64f7aba47f00f9f38826958c1b1
ialfai <k.zgodaferchmin@gmail.com>
2020-11-22 16:18:01 +0200

    ready flask version

--- zad6_flask.py
+++ zad6_flask.py
@@ -30,6 +30,21 @@ def dice(code):
             return dice_sum + z
 
 
+def truornot(a, b):
+    if a < 2001:
+        z = True
+    else:
+        z = False
+    if b < 2001:
+        y = True
+    else:
+        y = False
+    if z + y == 2:
+        return True
+    elif z + y < 2:
+        return False
+
+
 app = Flask('Witaj użytkowniku!')\
 
 @app.route('/')
@@ -40,19 +55,44 @@ def index():
 def gra2001():
 
     if request.method == 'GET':
-        return render_template('2001.html')
-
+        total_result = 0
+        computer_total = 0
+        return render_template('2001.html', total_result=total_result, computer_total=computer_total)
     else: # so it's POST
+        total_result = int(request.form['total_result'])
+        computer_total = int(request.form['computer_total'])
         dices = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
         throw_code1 = request.form['throw-code1']
         throw_code2 = request.form['throw-code2']
+        #dice throw for player
         dice_result1 = dice(throw_code1)
         dice_result2 = dice(throw_code2)
-        dice_result = str(dice_result1+dice_result2)
+        dice_result = dice_result1+dice_result2 # liczba
+        #dice throw for computer
         computer_dice_result1 = dice(dices[random.randint(0, 7)])
         computer_dice_result2 = dice(dices[random.randint(0, 7)])
-        computer_dice_result = computer_dice_result1 + computer_dice_result2
-        return render_template('2001.html', dice_result=dice_result, computer_dice_result=computer_dice_result)
+        computer_dice_result = computer_dice_result1 + computer_dice_result2 # liczba
+        #appending the total
+        if truornot(total_result, computer_total):
+            if dice_result == 7:
+                total_result = int(total_result/7)
+            elif dice_result == 11:
+                total_result = total_result * 11
+            else:
+                total_result += int(dice_result)
+            if computer_dice_result == 7:
+                computer_total = int(computer_total/7)
+            elif computer_dice_result == 11:
+                computer_total = computer_total * 11
+            else:
+                computer_total += int(computer_dice_result)
+            return render_template('2001.html', dice_result=dice_result, computer_dice_result=computer_dice_result,
+                               total_result=total_result,
+                               computer_total=computer_total)
+        else:
+            return render_template('2001.html',
+                               total_result=total_result,
+                               computer_total=computer_total, extra="We have a winner")
         # elif request.form['hint'] == 'too big':
         #     guess = int((maximum - minimum) / 2) + minimum
         #     return render_template('2001.html', minimum=minimum, maximum=guess, guess=guess)
